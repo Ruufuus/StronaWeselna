eaab8b81b536d29c65cdc75dcdf46a8db87e22b3
MisterSlimak <93765820+MisterSlimak@users.noreply.github.com>
2022-04-09 00:25:15 +0200

    Zmiana na singlepage

--- scripts.js
+++ scripts.js
@@ -1,37 +1,9 @@
-
 // DOM do node
 var p2NodeList = document.querySelectorAll('p2');
 var serchbar = document.querySelector('.guestSerchBar');
 
 // node do array
 var p2List = Array.from(p2NodeList);
-
-
-
-
-// let array = []
-
-// for(let i = 0 ; i < p2List.length; i++) {
-//     array.push(p2List[i].outerText)
-// }
-
-
-// console.log(p2NodeList)
-// console.log(p2List)
-// console.log('hi')
-// console.log("array",array)
- 
-
-// const object = {}
-// for(let i = 0 ; i < p2List.length; i++) {
-//     object[p2List[i].outerText] = "seat"+(i+1)
-//     serchbar.innerHTML += 
-//         "<input type=\"checkbox\" id=" +
-//         "\"" + "seat"+(i+1) + "\"/>" + 
-//         p2List[i].outerText + 
-//         "<br />"
-// }
-// console.log("object",object)
 
 
 // array of objects
@@ -56,13 +28,11 @@ function compare( a, b ) {
     return 0;
 }
 arrayObject.sort( compare );
-
-console.log("arrayObject",arrayObject)
+// console.log("arrayObject",arrayObject)
 
 
 // renderowanie listy
 for(let i = 0 ; i < arrayObject.length; i++) {
-    
     serchbar.innerHTML += 
         "<input type=\"checkbox\" onclick=\"addAtributeSeat(" +
         arrayObject[i].number + 
@@ -73,10 +43,8 @@ for(let i = 0 ; i < arrayObject.length; i++) {
         "<br />"
 }
 
+// podswietlanie osoby z listy
 function addAtributeSeat(numb) {
     let seat = document.querySelector('.seatHTML'+ numb)
-    // seat.setAttribute('id',"checkOn")
     seat.classList.toggle ('checkOn');
-
-    console.log("seat", seat)
 }
